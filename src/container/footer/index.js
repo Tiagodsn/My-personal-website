33c84fe438cb6a827fe59f7e9416938f8779bc88
Tiago Neves <tiagodasilvaneves@gmail.com>
2018-08-15 14:21:52 +0200

    Flow added to footer container

--- src/container/footer/index.js
+++ src/container/footer/index.js
@@ -1,34 +1,26 @@
-import React, { PureComponent } from "react";
+//@ flow
+import React from "react";
 
-class Footer extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.getYear = this.getYear.bind(this);
-  }
+const getYear = () => new Date().getFullYear();
 
-  getYear() {
-    const current = new Date();
+type Props = {}
 
-    return current.getFullYear();
-  }
+function footer(props: Props) {
+  const year = getYear();
 
-  render () {
-    const year = this.getYear();
-
-    return (
-      <footer id="footer">
-        <div className="container">
-          <div className="row justify-content-between">
-            <div className="col copyright">
-              { `© ${ year } Tiago Neves`}
-            </div>
-            <div className="col made-with-love">
-              made with <span>❤</span> in Düsseldorf
-            </div>
+  return (
+    <footer id="footer">
+      <div className="container">
+        <div className="row justify-content-between">
+          <div className="col copyright">
+            { `© ${ year } Tiago Neves`}
+          </div>
+          <div className="col made-with-love">
+            made with <span>❤</span> in Düsseldorf
           </div>
         </div>
-      </footer>
-    )
-  }
+      </div>
+    </footer>
+  )
 }
-export default Footer;
+export default footer;
