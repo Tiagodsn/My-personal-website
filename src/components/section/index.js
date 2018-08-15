68f01d2843f190b0cc7ad396c1263364cf76e233
Tiago Neves <tiagodasilvaneves@gmail.com>
2018-08-15 10:58:52 +0200

    Flow added to section component

--- src/components/section/index.js
+++ src/components/section/index.js
@@ -1,26 +1,30 @@
-import React, { PureComponent } from "react";
+// @flow
+import * as React from "react";
 
-class Section extends PureComponent {
-
-  render () {
-    const { id, title, children } = this.props;
-
-    return (
-      <section id={id}>
-          <div className="container">
-              <div className="row">
-                <div className="col-12 section-header">
-                    <h2>
-                      <span>{title}</span>
-                    </h2>
-                </div>
-                <div className="col-12">
-                  { children }
-                </div>
-              </div>
-          </div>
-      </section>
-    )
-  }
+type Props = {
+  id: string,
+  title: string,
+  children?: React.Node
 }
-export default Section;
+
+function section(props: Props) {
+  const { id, title, children } = props;
+
+  return (
+    <section id={id}>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 section-header">
+            <h2>
+              <span>{title}</span>
+            </h2>
+          </div>
+          <div className="col-12">
+            { children }
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+export default section;
